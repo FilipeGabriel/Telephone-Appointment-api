232c31b6d90461f20d06ff4d0ddae42b40c1d43c
Filipe Gabriel <filipegabriel11@hotmail.com>
2025-04-22 22:55:06 -0300

    First project commit

---
+++ src/test/java/io/filipegabriel/telephone_appointment_api/TelephoneAppointmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.filipegabriel.telephone_appointment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelephoneAppointmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
