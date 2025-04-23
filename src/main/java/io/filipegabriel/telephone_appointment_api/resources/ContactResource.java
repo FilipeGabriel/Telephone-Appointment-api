3f2e4d86ee9ca441c64f1974b5c8278b82e03fa2
Filipe Gabriel <filipegabriel11@hotmail.com>
2025-04-23 01:23:24 -0300

    feat - User and contacts resource created

---
+++ src/main/java/io/filipegabriel/telephone_appointment_api/resources/ContactResource.java
@@ -0,0 +1,9 @@
+package io.filipegabriel.telephone_appointment_api.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/api/contacts")
+public class ContactResource {
+}
